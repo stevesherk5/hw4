a9ff6cced4d00cb8a780a02c001a06bdf569d7d4
Steve Sherk <steven.sherk@kellogg.northwestern.edu>
2024-03-10 22:23:41 +0000

    still figuring out css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user["username"] = params["username"]
-    @user["email"] = params["email"]
-    @user["password"] = BCrypt::Password.create(params["password"])
-    @user.save
-    redirect_to "/login"
+    if User.find_by({ "username" => params["username"] }) == nil
+      @user = User.new
+      @user["username"] = params["username"]
+      @user["email"] = params["email"]
+      @user["password"] = BCrypt::Password.create(params["password"])
+      @user.save
+      redirect_to "/login"
+    else
+      flash["notice"] = "Username taken."
+      redirect_to "/users/new"
+    end
   end
 end
